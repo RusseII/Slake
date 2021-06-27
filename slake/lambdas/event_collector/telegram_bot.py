6af7c724723293ad1d88a1c26585d22ba733e0de
Russell <russell@deephire.com>
2021-06-27 06:17:43 -0500

    refactor secrets

--- slake/lambdas/event_collector/telegram_bot.py
+++ slake/lambdas/event_collector/telegram_bot.py
@@ -1,75 +1,14 @@
-import boto3
-import json
 import requests
 from enum import Enum
-
-
-# Use this code snippet in your app.
-# If you need more information about configurations or implementing the sample code, visit the AWS docs:
-# https://aws.amazon.com/developers/getting-started/python/
-
-import base64
-from botocore.exceptions import ClientError
+from secrets import get_secret
 
 
 chat_id = -475570287
 
+
 class Status(Enum):
     SINGLE = "awake"
     DOUBLE = "asleep"
-
-
-def get_secret():
-
-    secret_name = "russell_bot_api_key"
-    region_name = "us-west-2"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-            return json.loads(secret)['RUSSELL_BOT_API_KEY']
-        else:
-            decoded_binary_secret = base64.b64decode(
-                get_secret_value_response['SecretBinary'])
-            return json.loads(decoded_binary_secret)['RUSSELL_BOT_API_KEY']
 
 
 def send_telegram_message(status: Status):
@@ -81,7 +20,7 @@ def send_telegram_message(status: Status):
 
 
 def send(message: str):
-    RUSSELL_BOT_API_KEY = get_secret()
+    RUSSELL_BOT_API_KEY = get_secret(
+        "russell_bot_api_key", "RUSSELL_BOT_API_KEY")
     requests.post(f'https://api.telegram.org/bot{RUSSELL_BOT_API_KEY}/sendMessage', data={
-                      "chat_id": chat_id, "text": message})
-
+        "chat_id": chat_id, "text": message})
